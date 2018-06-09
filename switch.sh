dd441b9c19a2a198741021c98ba4c0208540076c
Jan Bölsche <regular.gonzales@googlemail.com>
2018-06-09 16:54:09 +0200

    Add bash script for testing GPIO wiring

---
+++ switch.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash -eux
+
+# GPIO out
+LED=1
+AMP=8
+gpio mode $LED out # LED
+gpio mode $AMP out # amp power
+
+# GPIO in
+ONSWITCH=4
+MODEBIT1=15
+MODEBIT2=16
+
+gpio mode $ONSWITCH up
+gpio mode $MODEBIT1 up
+gpio mode $MODEBIT2 up
+
+gpio read $ONSWITCH
+gpio read $MODEBIT1
+gpio read $MODEBIT2
+
+gpio write $LED `gpio read $ONSWITCH`
+gpio write $AMP `gpio read $ONSWITCH`
