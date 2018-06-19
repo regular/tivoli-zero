2bfe01c3d24328c4ab6bf1e4b642f5ffd14c8ce3
Jan Bölsche <regular.gonzales@googlemail.com>
2018-06-19 15:04:09 +0000

    switch.sh now robust when no station is currently playing

--- switch.sh
+++ switch.sh
@@ -21,15 +21,18 @@ b2=$(gpio read $MODEBIT2)
 
 mode="$b1$b2"
 mode_dec=$(echo "$((2#$mode))")
-echo "binary: $mode"
-echo "dec: $mode_dec"
+#echo "binary: $mode"
+echo "pos: $mode_dec"
 
 current=$(mpc status|head -n -1|tail -n1|cut -d\# -f2|cut -d/ -f1)
+if [ -z "$current" ]; then current="9"; fi
 echo "curr: $current"
 
-if [ $current -ne $mode_dec ]; then
+if [ $current -ne $mode_dec -a $b0 -gt 0 ]; then
   mpc  play $mode_dec
 fi
+
+mpc status
 
 gpio write $LED $b0
 gpio write $AMP $b0
